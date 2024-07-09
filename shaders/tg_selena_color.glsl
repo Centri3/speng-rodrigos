8a16da7e96d7acdce30a033500f805d14b6175cc
Centri3 <114838443+Centri3@users.noreply.github.com>
2024-07-09 14:16:59 -0500

    more colorVary changes

--- shaders/tg_selena_color.glsl
+++ shaders/tg_selena_color.glsl
@@ -117,7 +117,9 @@ vec4 ColorMapSelena(vec3 point, in BiomeData biomeData) {
     // Global albedo variations
 //RODRIGO - MODIFIED ALBEDO NOISE
     vec3 zz = (point + Randomize) * (0.5 * hillsFreq / (hillsMagn * hillsMagn));
-    noiseOctaves = 10.0;
+    noiseOctaves = 2.0;
+    noiseLacunarity = 2.3;
+    noiseH = 0.3;
     vec3 albedoVaryDistort = Fbm3D((point + Randomize) * 9.7) * (1.5 + venusMagn);
 
     vary = 1.0 - Fbm((point + albedoVaryDistort) + (9.5 - Fbm(zz) + Fbm(zz * 0.999)));
