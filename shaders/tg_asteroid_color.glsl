01494e2864da9ef9ae12becc9faa1476cc1f1197
Catherine Flores <catherine.3.flores@gmail.com>
2025-12-07 19:15:50 -0600

    asteroid test

--- shaders/tg_asteroid_color.glsl
+++ shaders/tg_asteroid_color.glsl
@@ -12,6 +12,7 @@ vec4    ColorMapAsteroid(vec3 point, float height, float slope)
         _hillsMagn = 0.05;
     }
 
+    noiseH = 0.1;
     noiseOctaves = 5.0;
     height = DistFbm(point * 3.7 + Randomize, 1.5);
 
@@ -20,6 +21,13 @@ vec4    ColorMapAsteroid(vec3 point, float height, float slope)
     p += Fbm3D(p * 0.5) * 1.2;
     float vary = saturate((Fbm(p) + 0.7) * 0.7);
 
+    // GlobalModifier // ColorVary setup
+    vec3 zz = (point + Randomize) * (0.0005 * hillsFreq / (_hillsMagn * _hillsMagn));
+	noiseOctaves = 14.0;
+    noiseH = smoothstep(0.0, 0.1, colorDistMagn) * 0.5;
+	vec3 albedoVaryDistort = Fbm3D((point + Randomize) * 0.07) * (1.5 + venusMagn);
+    vary = 1.0 - Fbm((point + albedoVaryDistort) * (1.5 - RidgedMultifractal(zz, 8.0)+ RidgedMultifractal(zz*0.999, 8.0)));
+
     // Scale detail texture UV and add a small distortion to it to fix pixelization
     vec2 detUV = (TexCoord.xy * faceParams.z + faceParams.xy) * texScale;
     noiseOctaves = 4.0;
@@ -27,6 +35,9 @@ vec4    ColorMapAsteroid(vec3 point, float height, float slope)
     detUV += shatterUV;
 
     Surface surf = GetBaseSurface(height, detUV);
+
+    // GlobalModifier // ColorVary apply
+	surf.color.rgb *= mix(colorVary, vec3(1.0), vary);
 
     surf.color.rgb *= 0.5 + slope;
     return surf.color;
