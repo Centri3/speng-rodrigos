3d09e05181c870ea2ed0cf2b357b963d82f3711c
Catherine Flores <catherine.3.flores@gmail.com>
2025-12-11 15:14:39 -0600

    clang-format

--- shaders/tg_asteroid_color.glsl
+++ shaders/tg_asteroid_color.glsl
@@ -4,44 +4,42 @@
 
 //-----------------------------------------------------------------------------
 
-vec4    ColorMapAsteroid(vec3 point, float height, float slope)
-{
-    float _hillsMagn = hillsMagn;
-    if (hillsMagn < 0.05)
-    {
-        _hillsMagn = 0.05;
-    }
+vec4 ColorMapAsteroid(vec3 point, float height, float slope) {
+  float _hillsMagn = hillsMagn;
+  if (hillsMagn < 0.05) {
+    _hillsMagn = 0.05;
+  }
 
-    noiseOctaves = 5.0;
-    height = DistFbm(point * 3.7 + Randomize, 1.5);
+  noiseOctaves = 5.0;
+  height = DistFbm(point * 3.7 + Randomize, 1.5);
 
-    noiseOctaves = 5.0;
-    vec3 p = point * colorDistFreq * 2.3;
-    p += Fbm3D(p * 0.5) * 1.2;
-    float vary = saturate((Fbm(p) + 0.7) * 0.7);
+  noiseOctaves = 5.0;
+  vec3 p = point * colorDistFreq * 2.3;
+  p += Fbm3D(p * 0.5) * 1.2;
+  float vary = saturate((Fbm(p) + 0.7) * 0.7);
 
-    // Scale detail texture UV and add a small distortion to it to fix pixelization
-    vec2 detUV = (TexCoord.xy * faceParams.z + faceParams.xy) * texScale;
-    noiseOctaves = 4.0;
-    vec2 shatterUV = Fbm2D2(detUV * 16.0) * (16.0 / 512.0);
-    detUV += shatterUV;
+  // Scale detail texture UV and add a small distortion to it to fix
+  // pixelization
+  vec2 detUV = (TexCoord.xy * faceParams.z + faceParams.xy) * texScale;
+  noiseOctaves = 4.0;
+  vec2 shatterUV = Fbm2D2(detUV * 16.0) * (16.0 / 512.0);
+  detUV += shatterUV;
 
-    Surface surf = GetBaseSurface(height, detUV);
+  Surface surf = GetBaseSurface(height, detUV);
 
-    surf.color.rgb *= 0.5 + slope;
-    return surf.color;
+  surf.color.rgb *= 0.5 + slope;
+  return surf.color;
 }
 
 //-----------------------------------------------------------------------------
 
-void main()
-{
-    vec3  point = GetSurfacePoint();
-    float height = 0, slope = 0;
-    GetSurfaceHeightAndSlope(height, slope);
-    OutColor = ColorMapAsteroid(point, height, slope);
-    OutColor.rgb = pow(OutColor.rgb, colorGamma);
-    //OutColor2 = vec4(height, slope, 0.0, 1.0);
+void main() {
+  vec3 point = GetSurfacePoint();
+  float height = 0, slope = 0;
+  GetSurfaceHeightAndSlope(height, slope);
+  OutColor = ColorMapAsteroid(point, height, slope);
+  OutColor.rgb = pow(OutColor.rgb, colorGamma);
+  // OutColor2 = vec4(height, slope, 0.0, 1.0);
 }
 
 //-----------------------------------------------------------------------------
--- shaders/tg_clouds_color.glsl
+++ shaders/tg_clouds_color.glsl
@@ -5,32 +5,32 @@
 //-----------------------------------------------------------------------------
 
 void main() {
-    float height = GetSurfaceHeight();
+  float height = GetSurfaceHeight();
 
-    float modulate = saturate(height * height * 3.5 + height * 4.5);
-    OutColor = GetCloudsColor(height);
+  float modulate = saturate(height * height * 3.5 + height * 4.5);
+  OutColor = GetCloudsColor(height);
 
-    // Venus-like clouds
-    if(cloudsCoverage == 1.0) {
-        if(cloudsLayer == 0) {
-            OutColor.a = 1.0;
-        } else {
-            OutColor.a *= modulate;
-        }
-
-        float latitude = abs(GetSurfacePoint().y);
-        // Drown out poles
-        // TODO: Occasionally make the equator drowned out, like Titan
-        OutColor.rgb = pow(OutColor.rgb, 1.0 - vec3(saturate(latitude - 0.4)));
-
-        if(cloudsLayer == 0.0 && cloudsNLayers != 1) {
-            OutColor.a = 0.0;
-        }
+  // Venus-like clouds
+  if (cloudsCoverage == 1.0) {
+    if (cloudsLayer == 0) {
+      OutColor.a = 1.0;
     } else {
-        OutColor *= modulate;
+      OutColor.a *= modulate;
     }
 
-    OutColor.rgb = pow(OutColor.rgb, colorGamma);
+    float latitude = abs(GetSurfacePoint().y);
+    // Drown out poles
+    // TODO: Occasionally make the equator drowned out, like Titan
+    OutColor.rgb = pow(OutColor.rgb, 1.0 - vec3(saturate(latitude - 0.4)));
+
+    if (cloudsLayer == 0.0 && cloudsNLayers != 1) {
+      OutColor.a = 0.0;
+    }
+  } else {
+    OutColor *= modulate;
+  }
+
+  OutColor.rgb = pow(OutColor.rgb, colorGamma);
 }
 
 //-----------------------------------------------------------------------------
