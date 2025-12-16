616e8ae12d99d9fe1e0853e722402b399a1d69dc
Catherine Flores <catherine.3.flores@gmail.com>
2025-12-15 19:57:28 -0600

    increase rivers octaves

--- shaders/tg_terra_height.glsl
+++ shaders/tg_terra_height.glsl
@@ -12,27 +12,29 @@ void _PseudoRivers(vec3 point, float damping, inout float height) {
   noiseH = 1.0;
   noiseLacunarity = 2.1;
 
-  vec3 p = point * mainFreq + Randomize;
-  vec3 distort = 0.325 * Fbm3D(p * riversSin);
-  distort = 0.65 * Fbm3D(p * riversSin) + 0.03 * Fbm3D(p * riversSin * 5.0) +
-            0.01 * RidgedMultifractalErodedDetail(
-                       point * 0.3 * (canyonsFreq + 1000) *
-                               (0.5 * (1 / montesSpiky + 1)) +
-                           Randomize,
-                       8.0, erosion, 2);
+  for (int i = 0; i < 3; i++) {
+    vec3 p = point * mainFreq + Randomize;
+    vec3 distort = 0.325 * Fbm3D(p * riversSin * i);
+    distort = 0.65 * Fbm3D(p * riversSin * i) + 0.03 * Fbm3D(p * riversSin * 5.0 * i) +
+              0.01 * RidgedMultifractalErodedDetail(
+                        point * 0.3 * (canyonsFreq + 1000) *
+                                (0.5 * (1 / montesSpiky + 1)) +
+                            Randomize,
+                        8.0, erosion, 2);
 
-  vec2 cell = 2.5 * Cell3Noise2(riversFreq * 3.0 * p + 0.5 * distort);
+    vec2 cell = 2.5 * Cell3Noise2(riversFreq * i * 3.0 * p + 0.5 * distort);
 
-  float errorcor =
-      pow(0.992, (1 / seaLevel)); // Correct Rivers on marine planets
+    float errorcor =
+        pow(0.992, (1 / seaLevel)); // Correct Rivers on marine planets
 
-  float valleys = 1.0 - (saturate(0.36 * abs(cell.y - cell.x) * riversMagn));
-  valleys = smoothstep(0.0, 1.0, valleys) * damping;
-  height = mix(height, seaLevel + 0.019 + errorcor * 0.042, valleys);
+    float valleys = 1.0 - (saturate(0.36 * abs(cell.y - cell.x) * riversMagn));
+    valleys = smoothstep(0.0, 1.0, valleys) * damping;
+    height = mix(height, seaLevel + 0.019 + errorcor * 0.042, valleys);
 
-  float rivers = 1.0 - (saturate(6.5 * abs(cell.y - cell.x) * riversMagn));
-  rivers = smoothstep(0.0, 1.0, rivers) * damping;
-  height = mix(height, seaLevel + 0.004 + errorcor * 0.052, rivers);
+    float rivers = 1.0 - (saturate(6.5 * abs(cell.y - cell.x) * riversMagn));
+    rivers = smoothstep(0.0, 1.0, rivers) * damping;
+    height = mix(height, seaLevel + 0.004 + errorcor * 0.052, rivers);
+  }
 }
 
 void _PseudoCracks(vec3 point, float damping, inout float height) {
