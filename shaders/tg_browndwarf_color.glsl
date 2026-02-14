c11edfa5e58df6114209d90ed88ed6b5f717aa56
Kevin <14kevinnymberg@gmail.com>
2026-02-14 12:20:24 -0500

    Add files via upload

--- shaders/tg_browndwarf_color.glsl
+++ shaders/tg_browndwarf_color.glsl
@@ -4,35 +4,19 @@
 
 //-----------------------------------------------------------------------------
 
-float   HeightMapFogGasGiant(vec3 point)
+void main()
 {
-    return 0.75 + 0.3 * Noise(point * vec3(0.2, stripeZones * 0.5, 0.2));
-}
-
-//-----------------------------------------------------------------------------
-
-void main() {
-    // GlobalModifier // Convert height to color
     float height = GetSurfaceHeight();
     OutColor = GetGasGiantCloudsColor(max(height, 1.0 - float(BIOME_CLOUD_LAYERS-1) / float(BIOME_SURF_LAYERS)))*0.3+0.4*GetGasGiantCloudsColor(min(height, 0.7 - float(BIOME_CLOUD_LAYERS-1) / float(BIOME_SURF_LAYERS)));
 	OutColor.rgb = (pow(OutColor.rgb, vec3(height)));
 
-	// GlobalModifier // Change cloud alpha channel
-	   // Changed lowest cloud layer to be full alpha // by Sp_ce
-	if (cloudsLayer == 0) {
-		//OutColor = GetGasGiantCloudsColor(height);
-		OutColor.a = 1.0; 
-	}
-	else {
-		float height = HeightMapFogGasGiant(GetSurfacePoint());
-        OutColor.rgb = height * GetGasGiantCloudsColor(1.0).rgb;
-        OutColor.a = 1.0;
-	}
+    //float a = cloudsLayer - height;
+    //OutColor.a = exp(-55.0 * a * a) * cloudsCoverage;
+    OutColor.a = 1.0 * dot(OutColor.rgb, vec3(0.299, 0.587, 0.114));
 
-	// GlobalModifier // Output color
     OutColor.rgb *= pow(OutColor.rgb, colorGamma);
+    //OutColor2 = vec4(0.0);
 }
-
 
 //-----------------------------------------------------------------------------
 
