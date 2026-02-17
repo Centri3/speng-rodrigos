71f88082371d4cd9d72d9cbdbc719f2cf3035ba4
Catherine Flores <catherine.3.flores@gmail.com>
2026-02-17 15:51:16 -0600

    WIP gas giant shaders

--- shaders/tg_gasgiant_height.glsl
+++ shaders/tg_gasgiant_height.glsl
@@ -5,18 +5,73 @@
 //-----------------------------------------------------------------------------
 
 vec3 TurbulenceGasGiantGmail(vec3 point) {
-  const float scale = 1.7;
-
   vec3 twistedPoint = point;
   vec3 cellCenter = vec3(0.0);
   vec2 cell;
   float r, fi, rnd, dist, dist2, dir;
-  float strength = 5.5;
-  float freq = 900 * scale;
-  float size = 14.0 * scale;
-  float dens = 0.8;
+  float strength = 9.5;
+  float freq = 80.0;
+  float size = 4.0;
+  float dens = 1.0;
+  vec3 randomize = Randomize;
 
-  for (int i = 0; i < 5; i++) {
+  // high-level turbulence
+
+  for (int i = 0; i < 1; i++) {
+    float angleY = randomize.y * 6.283185;
+
+    randomize.y = hash1(randomize.y);
+    size -= randomize.y * 0.1;
+
+    // clang-format off
+    mat3x3 rotY = mat3x3(cos(angleY), 0.0, sin(angleY),
+                          0.0, 1.0, 0.0,
+                          -sin(angleY), 0.0, cos(angleY));
+    // clang-format on
+
+    point *= rotY;
+
+    twistedPoint = point;
+    vec2 cell = inverseSF(point, freq, cellCenter);
+    rnd = hash1(cell.x);
+    r = size * cell.y;
+
+    if ((rnd < dens) && (r < 1.0)) {
+      dir = sign(0.5 * dens - rnd);
+      dist = saturate(1.0 - r);
+      dist2 = saturate(0.5 - r);
+      fi = pow(dist, strength) * (exp(-6.0 * dist2) + 0.25);
+      twistedPoint = Rotate(dir * stripeTwist * sign(cellCenter.y) * fi * 2.2,
+                            cellCenter.xyz, point);
+    }
+
+    freq *= 1.03;
+    size *= 1.03;
+    point = twistedPoint;
+  }
+
+  // mid-level turbulence
+
+  strength = 8.0 + (1.0 - lavaCoverage) * 2.0;
+  freq = 100.0 - 80.0 * lavaCoverage;
+  size = 11.0 - 8.0 * lavaCoverage;
+
+  for (int i = 0; i < 80; i++) {
+    float angleY = randomize.y * 0.03 + lavaCoverage * 0.97 * 6.283185;
+
+    randomize.y = hash1(randomize.y);
+    size -= randomize.y * 0.1;
+
+    // clang-format off
+    // TODO: Make a helper function for this!
+    mat3x3 rotY = mat3x3(cos(angleY), 0.0, sin(angleY),
+                          0.0, 1.0, 0.0,
+                          -sin(angleY), 0.0, cos(angleY));
+    // clang-format on
+
+    point *= rotY;
+
+    twistedPoint = point;
     vec2 cell = inverseSF(point, freq, cellCenter);
     rnd = hash1(cell.x);
     r = size * cell.y;
@@ -30,9 +85,8 @@ vec3 TurbulenceGasGiantGmail(vec3 point) {
                             cellCenter.xyz, point);
     }
 
-    freq = min(freq * 2.0, 1600.0);
-    size = min(size * 1.2, 30.0);
-    strength = strength * 1.5;
+    freq *= 1.03;
+    size *= 1.03;
     point = twistedPoint;
   }
 
@@ -89,11 +143,11 @@ float HeightMapCloudsGasGiantGmail(vec3 point, bool cyclones,
   vec3 twistedPoint = point;
 
   // Compute zones
-  float zones =
-      Noise(
-          vec3(0.0, twistedPoint.y * _stripeZones * 0.6 + cloudsLayer, 0.35)) *
-          0.8 +
-      0.20;
+  noiseOctaves = 15.0;
+  noiseH = 0.3;
+  noiseLacunarity = 4.0;
+  noiseH = 1.0 - lavaCoverage * 0.2;
+  noiseLacunarity = 1.4 + lavaCoverage;
   float offset = 0.0;
 
   // Compute cyclons
@@ -111,7 +165,7 @@ float HeightMapCloudsGasGiantGmail(vec3 point, bool cyclones,
   float height =
       unwrap_or(stripeFluct, 0.0) * 0.5 * (Fbm(twistedPoint) * 0.8 + 0.1);
 
-  return zones + height + offset;
+  return height + offset;
 }
 //-----------------------------------------------------------------------------
 
